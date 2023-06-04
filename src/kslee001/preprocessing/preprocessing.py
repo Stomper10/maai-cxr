7124c021188b410855984c77c61bcdedceedc6d1
kslee001 <ksnannaya@snu.ac.kr>
2023-06-04 23:52:38 +0900

    preprocessing --resize 512

---
+++ src/kslee001/preprocessing/preprocessing.py
@@ -0,0 +1,59 @@
+import os
+import glob
+import numpy as np
+import cv2
+from tqdm.auto import tqdm as tq
+from joblib import Parallel, delayed
+
+image_folder = "/home/n1/gyuseonglee/workspace/datasets/chexpert/CheXpert-v1.0"
+target_folder = "/home/n1/gyuseonglee/workspace/datasets/chexpert-resized-512"
+
+
+def make_dir(directory:str):
+    if not os.path.exists(directory):
+        os.makedirs(directory) 
+
+def center_crop(img, target_size=512):
+    h, w, c = img.shape
+    if (h<target_size) | (w<target_size):
+        set_size = max(h, w)
+    elif h>w:
+        set_size = w
+    else:
+        set_size = h
+    crop_width = set_size
+    crop_height = set_size
+    mid_x, mid_y = w//2, h//2
+    offset_x, offset_y = crop_width//2, crop_height//2
+    crop_img = img[mid_y - offset_y:mid_y + offset_y, mid_x - offset_x:mid_x + offset_x]
+    return crop_img
+
+def resize(img, target_size=512):
+    return cv2.resize(img, (target_size,target_size), interpolation=cv2.INTER_LANCZOS4)
+
+def process(pid, copy_from_list, copy_to_list, target_size=512):
+    img = cv2.imread(copy_from_list[pid])
+    img = resize(center_crop(img, target_size=target_size), target_size=target_size)
+    target_dir = copy_to_list[pid]
+    cv2.imwrite(target_dir, img.astype(np.uint8))
+
+
+if __name__ == '__main__':
+
+    num_patient = 65240    
+    target = num_patient+1
+
+    print("-- load image directories...")
+    d = sum([glob.glob(f"{image_folder}/train/patient{str(idx).zfill(5)}/study*/*.jpg") for idx in tq(range(1, target))], [])
+    d = [d[idx] for idx in range(len(d)) if 'lateral' not in d[idx]]
+
+    patients = [d[idx].split("v1.0/train/")[1].split("/study")[0] for idx in range(len(d))]
+    studies  = [d[idx].split(f"{patients[idx]}")[1].split("/view")[0].replace("/", "") for idx in range(len(d))]
+    filename = [d[idx].split(f"{studies[idx]}")[1].replace("/", "") for idx in range(len(d))]
+    
+    copy_from_list = d[:]
+    copy_to_list   = [f"{target_folder}/{patients[pid]}_{studies[pid]}_{filename[pid]}" for pid in range(len(d))]
+
+    print("-- process images...")
+    Parallel(n_jobs=-1)(delayed(process)(pid, copy_from_list, copy_to_list, 512)
+    for pid in tq(range(1, len(copy_from_list))))    
---
+++ src/kslee001/preprocessing/preprocessing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=preprocessing
+#SBATCH --nodes=1
+#SBATCH --nodelist=b02
+#SBATCH --gres=gpu:0
+#SBATCH --partition=3090
+#SBATCH --cpus-per-task=32
+#SBATCH --time=0-12:00:00
+#SBATCH --mem=20G
+
+source /home/${USER}/.bashrc
+source /home/${USER}/anaconda3/bin/activate
+conda activate tf
+
+srun python ./preprocessing.py
