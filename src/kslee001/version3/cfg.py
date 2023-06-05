ae7ffcadc0e80d632cd5be96060f66c7fe74df30
kslee001 <ksnannaya@snu.ac.kr>
2023-06-05 22:10:09 +0900

    change configuration

--- src/kslee001/version3/cfg.py
+++ src/kslee001/version3/cfg.py
@@ -4,6 +4,7 @@ from functions import make_dir
 
 configs = EasyDict()
 configs.seed = 1005
+
 # directory & wandb setting
 configs.data_dir = 100 # defined at runtime
 configs.dataset_name = 'CheXpert-v1.0'
@@ -13,11 +14,11 @@ configs.wandb_name = None # defined at runtime
 # training configuration
 configs.cutoff = None  # for test ('None' for full model training)
 configs.batch_size = 32
-configs.epochs = 30
+configs.epochs = 15
 configs.learning_rate = 0.0001
-configs.weight_decay = 0.0004, #  
-configs.beta_1 = 0.9,
-configs.beta_2 = 0.999,
+configs.weight_decay = 0.0004 #  
+configs.beta_1 = 0.9
+configs.beta_2 = 0.999
 configs.ema_momentum = 0.99
 configs.valid_ratio = 0.1
 configs.warm_up_rate = 0.1
@@ -33,14 +34,11 @@ configs.use_aux_information = True
 configs.auxiliary_columns = ['Sex', 'Age']
 configs.target_columns = ['Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Pleural Effusion']
 configs.num_classes = len(configs.target_columns)
-# configs.image_size = (384, 384)
 configs.image_size = (512, 512)
-
 
 # misc.
 configs.num_workers = 16
 configs.saved_model_path = "./best_model.h5"
-
 
 # data augmentation configuration
 configs.translation_height_factor = (-0.03, 0.03) # -3% ~ 3%
