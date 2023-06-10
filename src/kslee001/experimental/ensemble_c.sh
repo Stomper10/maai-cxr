e136367a54cda8da2a606c4685c6ac33507b038c
kslee001 <ksnannaya@snu.ac.kr>
2023-06-11 03:55:19 +0900

    ensemble shell

---
+++ src/kslee001/experimental/ensemble_c.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=experimental
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=16
+#SBATCH --time=0-12:00:00
+#SBATCH --mem=40000MB
+
+sbatch run_c.sh --label atel $@
+sbatch run_c.sh --label card $@
+sbatch run_c.sh --label cons $@
+sbatch run_c.sh --label edem $@
+sbatch run_c.sh --label plef $@
+
