e441e496cd64310204dfa8671a6924c2e970bbe9
kslee001 <ksnannaya@snu.ac.kr>
2023-06-10 13:30:13 +0900

    inference code

--- src/kslee001/final/inference.py
+++ src/kslee001/final/inference.py
@@ -5,6 +5,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="0,1,2,3"
 import logging
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
+import time as t
 import glob
 import argparse
 import pandas as pd
@@ -22,15 +23,15 @@ from cfg import configs
 import functions
 
 
-configs.model.backbone = 'densenet'
+# configs.model.backbone = 'densenet'
 # configs.model.backbone = 'convnext'
 
 if __name__ == '__main__':
-    weights = glob.glob(f"./{configs.model.backbone}*.h5")
     # argument parsing : [ cluster, backbone type (densenet, convnext), head ]
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--cluster', action='store', default='akmu')
-    # parser.add_argument('-b', '--backbone', action='store', default='densenet')
+    parser.add_argument('-b', '--backbone', action='store', default='densenet')
+    parser.add_argument('-s', '--seed', action='store', default=1005, type=int)
 
     args = parser.parse_args()
     cluster = args.cluster
@@ -40,7 +41,8 @@ if __name__ == '__main__':
         configs.dataset.data_dir = '/home/gyuseonglee/workspace/dataset/chexpert-resized'
     if cluster == 'akmu':
         configs.dataset.data_dir = '/data/s1/gyuseong/chexpert-resized'
-    # configs.model.backbone = args.backbone
+    configs.general.seed = int(args.seed)
+    configs.model.backbone = args.backbone
     configs.model.classifier.add_expert = False
     configs.dataset.cutoff = 10
     configs.wandb.use_wandb = False
@@ -48,7 +50,7 @@ if __name__ == '__main__':
     configs.general.distributed = False
     configs.general.epochs = 1
     configs.general.batch_size = 16
-    configs.saved_model_path = "./" + configs.model.backbone + "_best_model_{epoch:02d}-{val_loss:.2f}.h5" 
+    configs.saved_model_path = "./" + configs.model.backbone + "_{epoch:02d}-{val_loss:.2f}.h5" 
 
     # multi-gpu training strategy
     strategy = tf.distribute.MirroredStrategy()
@@ -62,7 +64,8 @@ if __name__ == '__main__':
     # load datasets
     train_dataset, valid_dataset, test_dataset = functions.load_datasets(configs) 
 
-
+    # load weights
+    weights = sorted(glob.glob(f"./{configs.model.backbone}_{configs.general.seed}*.h5"))
 
     best_weights = None
     best_auc = -1
@@ -73,16 +76,21 @@ if __name__ == '__main__':
             configs=configs,
             training=False,
         )
+        start_time = t.time()
+        losses = model.evaluate(test_dataset, verbose=0)
+        end_time = t.time()
+        duration = np.round(end_time-start_time, 6)
+        process_images_per_sec = np.round(202/duration, 6)
+        single_image_processing = np.round(1/process_images_per_sec, 6)
 
-        losses = model.evaluate(test_dataset)
         targets = ['atel', 'card', 'cons', 'edem', 'plef']
         average_auc = np.round(np.mean(losses[2:]), 4)
 
         # evaluation
-        print("-- result of : ", path)
+        print("[RESULT] of : ", path)
         print( str([f"val_loss : {np.round(losses[0], 4)}"] + [ f"{targets[idx]} : {np.round(losses[2:][idx], 4)}"  for idx in range(5) ]))
         print("-- average AUC : ", average_auc)
-
+        print(f"-- DURATION : {duration} | 1 image : {single_image_processing} | 1 sec : {process_images_per_sec}\n")
         if average_auc > best_auc:
             best_auc = average_auc
             best_weights = path
---
+++ src/kslee001/final/inference_ab_gpu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=inference
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+#SBATCH --partition=3090
+#SBATCH --cpus-per-task=16
+#SBATCH --time=0-12:00:00
+#SBATCH --mem=20000MB
+
+source /home/n1/${USER}/.bashrc
+source /home/n1/${USER}/anaconda3/bin/activate
+conda activate tf
+
+srun python ./inference.py --cluster gsds-ab $@
+
+
